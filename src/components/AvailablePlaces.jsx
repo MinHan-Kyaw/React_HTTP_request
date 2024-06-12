bb82c6addf8a5e6fcff76faa61cbcf39a524ac73
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-06-12 15:51:19 +0700

    Transforming Fetched Data

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -11,20 +12,27 @@ export default function AvailablePlaces({ onSelectPlace }) {
     async function fetchPlaces() {
       setIsFetching(true);
       try {
-        const response = await fetch("http://localhost:3000/placessss");
+        const response = await fetch("http://localhost:3000/places");
         const resData = await response.json();
         console.log(response);
         if (!response.ok) {
           throw new Error("Failed to fetch places!");
         }
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
         setError({
           message:
             error.message || "Could not fetch places, please try again later.",
         });
       }
-      setIsFetching(false);
     }
     fetchPlaces();
   }, []);
